03c1c70ef37742a8bfbf449ecb885c8d34783acc
Grubb <developer.grubb@gmail.com>
2022-03-05 17:15:00 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ import os
 
 __doc__ = """
 App for Django featuring improved form base classes.
+Forked from better-forms, then updated for python3 and django 4.0
 """
 
-version = '1.2.1.dev0'
+version = '2.0.0'
 
 
 def read(fname):
@@ -14,35 +15,32 @@ def read(fname):
 
 
 setup(
-	name='django-betterforms',
+	name='grubb-betterforms',
 	version=version,
 	description=__doc__,
 	long_description=read('README.rst'),
-	url="https://django-betterforms.readthedocs.org/en/latest/",
-	author="Fusionbox",
-	author_email='programmers@fusionbox.com',
+	url="https://github.com/chrisdotgrubb/django-betterforms",
+	author="grubb",
+	author_email='developer.grubb@gmail.com',
 	packages=[package for package in find_packages()
 			  if package.startswith('betterforms')],
-	install_requires=['Django>=1.8'],
+	install_requires=['Django>=3.0'],
 	zip_safe=False,
 	include_package_data=True,
 	classifiers=[
 		'Development Status :: 5 - Production/Stable',
-		'Framework :: Django',
-		'Framework :: Django :: 1.8',
-		'Framework :: Django :: 1.9',
-		'Framework :: Django :: 1.10',
-		'Framework :: Django :: 1.11',
-		'Framework :: Django :: 2.0',
+		'Framework :: Django :: 4.0',
 		'Intended Audience :: Developers',
 		'License :: OSI Approved :: BSD License',
 		'Programming Language :: Python',
-		'Programming Language :: Python :: 2',
-		'Programming Language :: Python :: 2.7',
 		'Programming Language :: Python :: 3',
 		'Programming Language :: Python :: 3.4',
 		'Programming Language :: Python :: 3.5',
 		'Programming Language :: Python :: 3.6',
+		'Programming Language :: Python :: 3.7',
+		'Programming Language :: Python :: 3.8',
+		'Programming Language :: Python :: 3.9',
+		'Programming Language :: Python :: 3.10',
 		'Topic :: Internet :: WWW/HTTP',
 	],
 )
